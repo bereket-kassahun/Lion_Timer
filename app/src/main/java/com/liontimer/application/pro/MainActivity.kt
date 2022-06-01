2c401438d9e16edb6e3d580d9a9859499cbe6cce
bereket-kassahun <bereket_kasahun@yahoo.com>
2022-06-01 16:56:13 +0300

    init & feat: initializing and partially building ui

---
+++ app/src/main/java/com/liontimer/application/pro/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.liontimer.application.pro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.main_container)
+    }
+}
---
+++ app/src/main/java/com/liontimer/application/pro/ui/main/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.liontimer.application.pro.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.liontimer.application.pro.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val root = inflater.inflate(R.layout.main_fragment, container, false)
+        return root
+    }
+
+}
